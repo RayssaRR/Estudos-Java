061752ac1d18e1708a070090104646a286825c34
RayssaRR <rayssa_snt@hotmail.com>
2026-08-22 17:12:29 -0300

    exercicios test

---
+++ src/main/java/estudos/logica/condicionais/ex01/ex01.java
@@ -0,0 +1,36 @@
+package estudos.logica.condicionais.ex01;
+import java.util.Scanner;
+/**
+ * Exercício 1 — Positivo, negativo ou zero
+ * Peça ao usuário para digitar um número inteiro.
+ * Seu programa deve informar:
+ * Se o número for maior que 0 → "Positivo"
+ * Se for menor que 0 → "Negativo"
+ * Se for igual a 0 → "Zero"
+ * Exemplos:
+ * Digite um número: 15
+ * Resultado: Positivo
+ * Digite um número: -8
+ * Resultado: Negativo
+ */
+
+public class ex01 {
+
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Informe um número inteiro");
+        int numero = scanner.nextInt();
+        if(numero>0){
+            System.out.println("Positivo");
+        }else if(numero<0){
+            System.out.println("Negativo");
+        }else{
+            System.out.println("Zero");
+        }
+
+        scanner.close();
+
+    }
+
+
+}
